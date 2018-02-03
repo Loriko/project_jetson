ab1da4d69aa8f8b8a48b1cc9da237f3c4fa6216b
Mohamed Ali Ramadan <mrama030@uottawa.ca>
2018-02-02 19:14:35 -0500

    diagram architecture

--- Back-End/WebServicesApp/WebServicesApp/Controllers/MainController.cs
+++ Back-End/WebServicesApp/WebServicesApp/Controllers/MainController.cs
@@ -11,8 +11,14 @@ namespace WebServicesApp.Controllers
     [Route("api/[controller]")]
     public class MainController : ControllerBase
     {
+
+        /// <summary>
+        /// Dedicated GET request type Front-End.
+        /// </summary>
+        /// <returns></returns>
         // GET: api/<controller>
         [HttpGet]
+        [Consumes("application/json")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
@@ -25,22 +31,30 @@ namespace WebServicesApp.Controllers
             return "value";
         }
 
+        
         // POST api/<controller>
         [HttpPost]
         public void Post([FromBody]string value)
         {
+
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+
         }
     }
 }
