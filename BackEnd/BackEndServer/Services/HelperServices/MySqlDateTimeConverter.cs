45bb600eae747c344182a72c9b8c9794a8d2ce81
Mohamed Ali Ramadan <mrama030@uottawa.ca>
2018-04-07 15:28:32 -0400

    CheckIfSQLFormat() implemented, Helper Class Unit Tests Pass (API_1 to API_3)

--- BackEnd/BackEndServer/Services/HelperServices/MySqlDateTimeConverter.cs
+++ BackEnd/BackEndServer/Services/HelperServices/MySqlDateTimeConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BackEndServer.Services.HelperServices
@@ -9,17 +10,21 @@ namespace BackEndServer.Services.HelperServices
 
     public static class MySqlDateTimeConverter
     {
+        // [API_UnitTest_1] - PASS (Mohamed R.)
         public static bool CheckIfSQLFormat(this string dateTimeString)
         {
-            // TO IMPLEMENT
-            throw new NotImplementedException();
+            Regex regex = new Regex(@"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$");
+
+            return regex.IsMatch(dateTimeString);
         }
 
+        // [API_UnitTest_2] - PASS (Mohamed R.)
         public static string toMySqlDateTime(this DateTime time)
         {
-            return time.ToString("yyyy-MM-dd HH:mm:ss");
+            return time.ToString("yyyy-MM-dd HH:mm:ss"); 
         }
 
+        // [API_UnitTest_3] - PASS (Mohamed R.)
         public static DateTime toDateTime(this string mySqlStringDate)
         {
             DateTime result = DateTime.Parse(mySqlStringDate);
