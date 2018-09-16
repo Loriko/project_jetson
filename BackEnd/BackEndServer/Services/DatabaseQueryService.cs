a70d98afc0891cdf2fc37c779f1bc08268fe0005
Francis <loriko1989@gmail.com>
2018-09-16 15:45:10 -0400

    #125: A PerSecondStat set in the future will now only be counted once for alert monitoring since we now check when grabbing all the applicable PerSecondStats that the PerSecondStat is set before a certain date. #270: Reintegrates a reverted commit #267: Sets start_time and end_time as null if always active

--- BackEnd/BackEndServer/Services/DatabaseQueryService.cs
+++ BackEnd/BackEndServer/Services/DatabaseQueryService.cs
@@ -995,7 +995,8 @@ namespace BackEndServer.Services
             return false;
         }
 
-        public DatabasePerSecondStat GetEarliestPerSecondStatTriggeringAlert(DatabaseAlert alert, DateTime lastUpdatedTime)
+        public DatabasePerSecondStat GetEarliestPerSecondStatTriggeringAlert(DatabaseAlert alert,
+            DateTime lastUpdatedTime, DateTime checkupDateTime)
         {
             DatabasePerSecondStat perSecondStat = null;
             TriggerOperator triggerOperator = (TriggerOperator)Enum.Parse(typeof(TriggerOperator), alert.TriggerOperator, true);
@@ -1006,7 +1007,8 @@ namespace BackEndServer.Services
                                $"WHERE {DatabasePerSecondStat.NUM_DETECTED_OBJECTS_LABEL} " +
                                $"{triggerOperator.GetSqlForm()} {alert.TriggerNumber} " +
                                $"AND {DatabasePerSecondStat.CAMERA_ID_LABEL} = {alert.CameraId} " +
-                               $"AND {DatabasePerSecondStat.DATE_TIME_LABEL} > STR_TO_DATE('{lastUpdatedTime}', '%m/%d/%Y %H:%i:%s')";
+                               $"AND {DatabasePerSecondStat.DATE_TIME_LABEL} > STR_TO_DATE('{lastUpdatedTime}', '%m/%d/%Y %H:%i:%s')" +
+                               $"AND {DatabasePerSecondStat.DATE_TIME_LABEL} < STR_TO_DATE('{checkupDateTime}', '%m/%d/%Y %H:%i:%s')"; //TODO: maybe get date right now from another parameter
                 
                 if (alert.SnoozedUntil != null)
                 {
