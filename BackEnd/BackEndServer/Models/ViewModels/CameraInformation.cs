c085770abe919b8bc1fa9ece46b1beb14e1cbf45
Umar Rehmani <umarrehmani@Umars-MacBook-Pro.local>
2018-09-18 20:39:38 -0400

    fix merge

--- BackEnd/BackEndServer/Models/ViewModels/CameraInformation.cs
+++ BackEnd/BackEndServer/Models/ViewModels/CameraInformation.cs
@@ -8,37 +8,21 @@ namespace BackEndServer.Models.ViewModels
         public int CameraId { get; set; }
         public string CameraRoomName { get; set; }
         public string CameraName { get; set; }
-        public int CameraUserId { get; set; }
         public GraphStatistics GraphStatistics { get; set; }
 
-<<<<<<< HEAD
-        public CameraInformation(int cameraId, string cameraRoomName, int cameraUserId){
+        public CameraInformation(int cameraId, string cameraRoomName){
             this.CameraId = cameraId;
             this.CameraRoomName = cameraRoomName;
             this.CameraName = cameraRoomName + " Camera";
-            this.CameraUserId = cameraUserId;
-=======
-        public CameraInformation(int cameraId, string cameraRoomName){
-            CameraId = cameraId;
-            CameraRoomName = cameraRoomName;
-            CameraName = cameraRoomName + " Camera";
->>>>>>> master
         }
 
-        public CameraInformation(int cameraId, string cameraRoomName, string cameraName, int cameraUserId)
+        public CameraInformation(int cameraId, string cameraRoomName, string cameraName)
         {
-<<<<<<< HEAD
             this.CameraId = cameraId;
             this.CameraRoomName = cameraRoomName;
             this.CameraName = cameraName;
-            this.CameraUserId = cameraUserId;
-=======
-            CameraId = cameraId;
-            CameraRoomName = cameraRoomName;
-            CameraName = cameraName;
->>>>>>> master
         }
 
-        public CameraInformation(DatabaseCamera dbCamera) : this(dbCamera.CameraId, dbCamera.MonitoredArea, dbCamera.CameraName, dbCamera.UserId) {}
+        public CameraInformation(DatabaseCamera dbCamera) : this(dbCamera.CameraId, dbCamera.MonitoredArea, dbCamera.CameraName) {}
     }
 }
