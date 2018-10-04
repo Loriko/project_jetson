818189b09d85034d49038c86c431670f6a729d97
Francis <loriko1989@gmail.com>
2018-10-04 03:40:38 -0400

    #156: Fix for linux based systems

--- BackEnd/BackEndServer/Services/HelperServices/RootDirectoryTools.cs
+++ BackEnd/BackEndServer/Services/HelperServices/RootDirectoryTools.cs
@@ -7,12 +7,21 @@ namespace BackEndServer.Services.HelperServices
     {
         public static string GetWWWRootPhysicalPath()
         {
+            string threeParentsLevelUp;
+            if (IsLinux)
+            {
+                threeParentsLevelUp = @"../../../";
+            }
+            else
+            {
+                threeParentsLevelUp = @"..\..\..\";
+            }
             return Path.Combine
             (
                 // "<PROJECT_ROOT>\\BackEnd\\BackEndServer\\bin\\Debug\\netcoreapp2.0\\"
                 AppDomain.CurrentDomain.BaseDirectory, 
                 // Go up 3 parent levels.
-                @"..\..\..\",
+                threeParentsLevelUp,
                 // Add the wwwroot child directory of the BackEndServer parent directory.
                 @"wwwroot"
             );
@@ -31,6 +40,16 @@ namespace BackEndServer.Services.HelperServices
         public static string GetWWWRootTempFolderVirtualPathForHTML()
         {
             return Path.Combine(GetWWWRootVirtualPathForHTML(), @"temp");
+        }
+        
+        //From accepted answer at https://stackoverflow.com/questions/5116977/how-to-check-the-os-version-at-runtime-e-g-windows-or-linux-without-using-a-con
+        public static bool IsLinux
+        {
+            get
+            {
+                int p = (int) Environment.OSVersion.Platform;
+                return p == 4 || p == 6 || p == 128;
+            }
         }
     }
 }
