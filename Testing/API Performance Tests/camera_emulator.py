411846c37b21bdb79a1f0feca5ffd114ff3d2376
Francis <loriko1989@gmail.com>
2018-10-13 02:35:58 -0400

    #206: Addition of API performance testing tool + small fix to api stat validation

---
+++ Testing/API Performance Tests/camera_emulator.py
@@ -0,0 +1,45 @@
+from jetson_stats_sender import PerSecondStats
+from jetson_stats_sender import http_service
+import datetime
+import time
+import random
+
+
+class CameraEmulator:
+    def __init__(self, camera_key, api_key, destination_address, delay_between_stats_sec):
+        http_service.initialize_stats_sender(api_key, destination_address)
+        self.camera_key = camera_key
+        self.delay_between_stats_sec = delay_between_stats_sec
+        self.stats_sent = 0
+        self.stats_acked = 0
+        self.stopped = False
+
+    def get_error_rate(self):
+        return (self.stats_sent - self.stats_acked) / float(self.stats_sent) if self.stats_sent != 0 else 0.0
+
+    def start(self):
+        generated_random_per_second_stat = PerSecondStats(self.camera_key, datetime.datetime.now(), 0, False)
+        self.send_per_second_stat(generated_random_per_second_stat)
+        while not self.stopped:
+            time.sleep(self.delay_between_stats_sec)
+            generated_random_per_second_stat = generate_random_per_second_stat(generated_random_per_second_stat)
+            self.send_per_second_stat(generated_random_per_second_stat)
+
+    def stop(self):
+        self.stopped = True
+
+    def send_per_second_stat(self, per_second_stat):
+        self.stats_sent = self.stats_sent + 1
+        if http_service.send_per_second_stat(per_second_stat):
+            self.stats_acked = self.stats_acked + 1
+
+
+# We take the previous per_second_stat and use it to calculate the next one.
+# We do this by adding or substracting a random but reasonable amount from the previous people_count
+def generate_random_per_second_stat(previous_per_second_stat):
+    previous_people_count = previous_per_second_stat.num_tracked_people
+    people_count_variation = random.choice((-4, -3, -2, -2, -2, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5))
+    new_people_count = previous_people_count + people_count_variation
+    if new_people_count < 0:
+        new_people_count = 0
+    return PerSecondStats(previous_per_second_stat.camera_key, datetime.datetime.now(), new_people_count, False)
---
+++ Testing/API Performance Tests/performance_test_suite.py
@@ -0,0 +1,70 @@
+import unittest
+from performance_tests_support import run_cameras_in_parallel
+
+
+ACCEPTED_ERROR_RATE_THRESHOLD = 0.02
+
+
+class RandomBasedCameraEmulatorTests(unittest.TestCase):
+    def test_1_camera_100ms_delay(self):
+        cameras = run_cameras_in_parallel(1, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_5_cameras_in_parallel_100ms_delay(self):
+        cameras = run_cameras_in_parallel(5, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_10_cameras_in_parallel_100ms_delay(self):
+        cameras = run_cameras_in_parallel(10, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_15_cameras_in_parallel_100ms_delay(self):
+        cameras = run_cameras_in_parallel(15, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_20_cameras_in_parallel_100ms_delay(self):
+        cameras = run_cameras_in_parallel(20, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_25_cameras_in_parallel_100ms_delay(self):
+        cameras = run_cameras_in_parallel(25, 0.1)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_1_camera_10ms_delay(self):
+        cameras = run_cameras_in_parallel(1, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_5_cameras_in_parallel_10ms_delay(self):
+        cameras = run_cameras_in_parallel(5, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_10_cameras_in_parallel_10ms_delay(self):
+        cameras = run_cameras_in_parallel(10, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_15_cameras_in_parallel_10ms_delay(self):
+        cameras = run_cameras_in_parallel(15, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_20_cameras_in_parallel_10ms_delay(self):
+        cameras = run_cameras_in_parallel(20, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+    def test_25_cameras_in_parallel_10ms_delay(self):
+        cameras = run_cameras_in_parallel(25, 0.01)
+        for camera in cameras:
+            self.assertLessEqual(camera.get_error_rate(), ACCEPTED_ERROR_RATE_THRESHOLD)
+
+if __name__ == '__main__':
+    unittest.main()
